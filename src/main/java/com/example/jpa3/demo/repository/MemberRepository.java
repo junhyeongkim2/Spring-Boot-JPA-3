d007fffe8b41291701df470267efa731453447fa
jun <ggprgrkjh@naver.com>
2023-06-18 17:26:05 +0900

    feat: findByPage

--- src/main/java/com/example/jpa3/demo/repository/MemberRepository.java
+++ src/main/java/com/example/jpa3/demo/repository/MemberRepository.java
@@ -2,7 +2,13 @@ package com.example.jpa3.demo.repository;
 
 import com.example.jpa3.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    //@Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 
 }
