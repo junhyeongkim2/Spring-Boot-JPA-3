66063bad7901068f1c28b82bc93fde2c09a32fb6
jun <ggprgrkjh@naver.com>
2023-06-18 18:28:37 +0900

    feat: jap paging and test

--- src/main/java/com/example/jpa3/demo/repository/MemberRepository.java
+++ src/main/java/com/example/jpa3/demo/repository/MemberRepository.java
@@ -1,14 +1,24 @@
 package com.example.jpa3.demo.repository;
 
 import com.example.jpa3.demo.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     //@Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+
+    Page<Member> findByAge(int age, PageRequest pageRequest);
+
+
+
+
 
 }
--- src/test/java/com/example/jpa3/demo/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/jpa3/demo/repository/MemberRepositoryTest.java
@@ -6,6 +6,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -70,6 +73,34 @@ public class MemberRepositoryTest {
         List<Member> result = memberRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
+    }
+
+
+    @Test
+    public void paging(){
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",10));
+        memberRepository.save(new Member("member3",10));
+        memberRepository.save(new Member("member4",10));
+        memberRepository.save(new Member("member5",10));
+
+        int age = 10;
+
+        PageRequest pageRequest = PageRequest.of(0,3, Sort.by(Sort.Direction.DESC,"username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age,pageRequest);
+
+        //then
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(6);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
 
 
     }
