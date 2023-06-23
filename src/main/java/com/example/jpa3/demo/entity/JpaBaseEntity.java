313c718edf1dc87a365f2752f5aa75f8c93e7678
jun <ggprgrkjh@naver.com>
2023-06-23 23:21:47 +0900

    feat: JpaBaseEntity Test

--- src/main/java/com/example/jpa3/demo/entity/JpaBaseEntity.java
+++ src/main/java/com/example/jpa3/demo/entity/JpaBaseEntity.java
@@ -1,10 +1,18 @@
 package com.example.jpa3.demo.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
+
+
+
+@Getter
+@MappedSuperclass
 public class JpaBaseEntity {
 
     @Column(updatable = false)
--- src/test/java/com/example/jpa3/demo/entity/MemberTest.java
+++ src/test/java/com/example/jpa3/demo/entity/MemberTest.java
@@ -1,6 +1,8 @@
 package com.example.jpa3.demo.entity;
 
+import com.example.jpa3.demo.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity(){
@@ -51,4 +56,29 @@ class MemberTest {
         }
 
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception{
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist 발생
+        
+        Thread.sleep(100);
+        member.setUsername("member2");
+        
+        em.flush(); //@PreUpdate
+        em.clear();
+        
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        System.out.println("findMember = " + findMember.getCreatedDate());
+        System.out.println("findMember = " + findMember.getUpdatedDate());
+
+
+
+    }
+
+
+
 }
